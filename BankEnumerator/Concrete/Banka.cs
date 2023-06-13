7cc6c243d0faffeedf409e118aca32359f4fa616
omerfdev <omerfdev@gmail.com>
2023-06-13 10:26:09 +0300

    Commit

--- BankEnumerator/Concrete/Banka.cs
+++ BankEnumerator/Concrete/Banka.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BankEnumerator.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,9 @@ namespace BankEnumerator.Concrete
 {
     internal class Banka : IBanka
     {
+        public delegate void HaberTipi(object sender);
+
+        //todo:1.)Delegate tanımlanır.public delegate void HaberTipi(object sender)
         public Banka()
         {
             Giseler=new List<IGise>();
@@ -18,15 +22,21 @@ namespace BankEnumerator.Concrete
         public INumarator Numarator { get; set ; }
         public ISayac Sayac { get ; set ; }
         public bool MesaiMi { get; set ; }
+        public static IMusteriDatabaseYöneticisi DBMusteri = new Database();
         public void MesaiBaslat(int giseSayisi)
         {
             Kuyruk=new Kuyruk();
             Sayac = new Sayac();
             Numarator=new Numarator();
+            if (giseSayisi>3) { throw new Exception("En fazla oluşturabilirsin..."); }
             for (int i = 0; i < giseSayisi; i++)
             {
                 IGise gise=new Gise();
+                //TODO:KUYRUKBENMÜSAİTİM, SAYAC BENİM İŞİM BİTTİ..
+                Giseler.Add(gise);
+
             }
+            MesaiMi=true;
         }
 
         public void MesaiBitir()
---
+++ BankEnumerator/Concrete/Database.cs
@@ -0,0 +1,22 @@
+﻿using BankEnumerator.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEnumerator.Concrete
+{
+    public class Database : IMusteriDatabaseYöneticisi
+    {
+        public Database()
+        {
+            MusteriTCleri = new List<string> { "1111", "2222","3333","4444","5555"};
+        }
+        public List<string> MusteriTCleri { get; set; }
+        public bool VipMi(string TC)
+        {
+            return MusteriTCleri.Contains(TC);
+        }
+    }
+}
