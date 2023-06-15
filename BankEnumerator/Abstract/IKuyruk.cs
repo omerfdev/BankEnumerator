17f0bad3228a4de38d12b474bd21bfaf08e73275
omerfdev <omerfdev@gmail.com>
2023-06-15 22:53:14 +0300

    Commit

--- BankEnumerator/Abstract/IKuyruk.cs
+++ BankEnumerator/Abstract/IKuyruk.cs
@@ -1,6 +1,16 @@
-﻿namespace BankEnumerator
+﻿using BankEnumerator.Abstract;
+using static BankEnumerator.Concrete.Banka;
+
+namespace BankEnumerator
 {
     public interface IKuyruk
     {
+        event NumaraHaberTipi NumaraAtadim;
+        List<INumara> VipNumaraListesi { get; set; }
+        List<INumara> BireyselNumaraListesi { get; set; }   
+        List<INumara> GiseNumaraListesi { get; set; }
+        int sayac { get; set; }
+        void NumaratordenNumaraAl(INumara numara);
+        void GiseyeNumaraGonder(object sender);
     }
 }
--- BankEnumerator/Concrete/Kuyruk.cs
+++ BankEnumerator/Concrete/Kuyruk.cs
@@ -1,6 +1,43 @@
-﻿namespace BankEnumerator
+﻿using BankEnumerator.Abstract;
+using BankEnumerator.Concrete;
+
+namespace BankEnumerator
 {
     public class Kuyruk : IKuyruk
     {
+        public Kuyruk()
+        {
+            VipNumaraListesi=new List<INumara> ();
+            BireyselNumaraListesi = new List<INumara> ();
+            GiseNumaraListesi= new List<INumara> ();
+            sayac = 0;
+        }
+        public List<INumara> VipNumaraListesi { get ; set ; }
+        public List<INumara> BireyselNumaraListesi { get; set ; }
+        public List<INumara> GiseNumaraListesi { get ; set ; }
+        public int sayac { get; set; }
+
+        public event Banka.NumaraHaberTipi NumaraAtadim;
+
+        public void GiseyeNumaraGonder(object sender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NumaratordenNumaraAl(INumara numara)
+        {
+            if (numara is VipNumarasi)
+            {
+                VipNumaraListesi.Add(numara);
+            }
+            else if (numara is BireyselNumara)
+            {
+                BireyselNumaraListesi.Add(numara);
+            }
+            else if (numara is GiseNumarasi)
+            {
+                GiseNumaraListesi.Add(numara);
+            }
+        }
     }
 }
